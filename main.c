d494631d1e85929e10914e0137604e4fb19c3fee
Joaov511 <jv232479@gmail.com>
2025-04-02 20:06:38 -0300

    add operations with digits moved to header file

--- main.c
+++ main.c
@@ -1,16 +1,26 @@
 #include<stdio.h>
 #include <stdint.h>
-
-void getNumberOfDigits();
-void concat();
-void concatDigits();
-void reverseInteger();
-void traverseThroughNumber();
-
+#include "digitsOperations.h"
 
 int main() {
+    int traverseThroughNumber(int x, int numberOfDigits, int *array) {
+        int divider = 10;
+        for(int i = 0; i <= numberOfDigits; i++) {
+            int digit = x % divider;
+            x = x/divider;
+            array[i] = digit;
+        }
+    }
+    
+    int reverseInteger(int x, int *array) {
+        int numberOfDigits = getNumberOfDigits(x);
+        int result = traverseThroughNumber(x, numberOfDigits, array);
+        int reversedInteger = concatDigits(numberOfDigits, array);
+        return reversedInteger;
+    }
+
     int numbers[10];
-    int x = 1000004423;
+    int x = 102345;
 
     int reversedInteger = reverseInteger(x, numbers);
     printf("%i", reversedInteger);
@@ -19,45 +29,6 @@ int main() {
 }
 
 
-int getNumberOfDigits(int number) {
-    int numberOfDigits = 0;
-    int divider = 10;
-    while(number > 0) {
-        number = number/divider;
-        numberOfDigits++;
-    }
-    return numberOfDigits;
-}
 
-int concat(int x, int y) {
-    int temp = y;
-    while (y != 0) {
-        x *= 10;
-        y /= 10;
-    }
-    return x + temp;
-}
 
-int concatDigits(int arrayLength, int *array) {
-    int numberConcat = array[0];
-    for(int i = 0; i < arrayLength ; i++) {
-        numberConcat = concat(numberConcat, array[i+1]);
-    }
-    return numberConcat;
-}
 
-int traverseThroughNumber(int x, int numberOfDigits, int *array) {
-    int divider = 10;
-    for(int i = 0; i <= numberOfDigits; i++) {
-        int digit = x % divider;
-        x = x/divider;
-        array[i] = digit;
-    }
-}
-
-int reverseInteger(int x, int *array) {
-    int numberOfDigits = getNumberOfDigits(x);
-    int result = traverseThroughNumber(x, numberOfDigits, array);
-    int reversedInteger = concatDigits(numberOfDigits, array);
-    return reversedInteger;
-}
