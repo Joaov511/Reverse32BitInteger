74d084ec80d2f3692a3f951b25d0265f9a1d7a3a
Joaov511 <jv232479@gmail.com>
2025-04-03 11:18:48 -0300

    refactor return type of traverse function

--- main.c
+++ main.c
@@ -2,31 +2,39 @@
 #include <stdint.h>
 #include "digitsOperations.h"
 
+void traverseThroughNumber(int x, int numberOfDigits, int *array);
+int reverseInteger(int x, int *array);
+
 int main() {
-    int traverseThroughNumber(int x, int numberOfDigits, int *array) {
-        int divider = 10;
-        for(int i = 0; i <= numberOfDigits; i++) {
-            int digit = x % divider;
-            x = x/divider;
-            array[i] = digit;
-        }
-    }
-    
-    int reverseInteger(int x, int *array) {
-        int numberOfDigits = getNumberOfDigits(x);
-        int result = traverseThroughNumber(x, numberOfDigits, array);
-        int reversedInteger = concatDigits(numberOfDigits, array);
-        return reversedInteger;
-    }
 
     int numbers[10];
-    int x = 102345;
+    int x = 10;
 
     int reversedInteger = reverseInteger(x, numbers);
     printf("%i", reversedInteger);
 
     return 0;
 }
+
+void traverseThroughNumber(int x, int numberOfDigits, int *array) {
+    int divider = 10;
+    for(int i = 0; i <= numberOfDigits; i++) {
+        int digit = x % divider;
+        x = x/divider;
+        array[i] = digit;
+    }
+}
+
+int reverseInteger(int x, int *array) {
+    int numberOfDigits = getNumberOfDigits(x);
+    traverseThroughNumber(x, numberOfDigits, array);
+    int reversedInteger = concatDigits(numberOfDigits, array);
+    return reversedInteger;
+}
+
+
+
+
 
 
 
