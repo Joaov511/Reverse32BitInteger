5f46f533ef7f2bdb79bdf73339fee3ca7ec56174
Joao Vitor Cunha Von Ah <admin.ah@fiecalunos.lan>
2025-04-08 20:34:28 -0300

    refactor number to be reversed now being passed as an argument

--- main.c
+++ main.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include "digitsOperations.h"
 
 int reverseInteger(int x, int *array);
 
-int main() {
+int main(int argc, char *argv[]) {
     int numbers[10];
-    int x = 1012345;
-    int reversedInteger = reverseInteger(x, numbers);
+    int number = atoi(argv[1]);
+    
+    int reversedInteger = reverseInteger(number, numbers);
     printf("%i", reversedInteger);
-
     return 0;
 }
 
